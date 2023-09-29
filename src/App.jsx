cb1b0bbc35dcd9d89df47e21c03f735e0bc074c5
amy0h <amy0h@gmail.com>
2023-09-30 01:54:51 +1300

    fixed layout issues on smalles screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -41,34 +41,40 @@ function App() {
   }
 
   return (
-    <main>
-      <img className='blob-top' src='/1.svg' alt='Top Blob' />
-      {!quizStatus ? (
-        <div>
-          <h1 className='title'>Quizzical</h1>
-          <p className='desc'>Explore Your Knowledge</p>
-          <button className='start-btn' onClick={handleShowModal}>
-            Ready to Play?
-          </button>
-        </div>
-      ) : (
-        <QuizPage
-          quizOption={quizOption}
-          setQuizStatus={setQuizStatus} 
-        />
-      )}
+    <div className='main-content-container'>
+      <main>
+        <img className='blob-top' src='/1.svg' alt='Top Blob' />
+        {!quizStatus ? (
+          <div>
+            <h1 className='title'>Quizzical</h1>
+            <p className='desc'>Explore Your Knowledge</p>
+            <button className='start-btn' onClick={handleShowModal}>
+              Ready to Play?
+            </button>
+          </div>
+        ) : (
+          <QuizPage
+            quizOption={quizOption}
+            setQuizStatus={setQuizStatus} 
+          />
+        )}
 
-      {showModal && (
-        <QuizSetup
-          quizOption={quizOption}
-          setQuizOption={setQuizOption}
-          setQuizStatus={setQuizStatus}
-          toggleShowModal={toggleShowModal}
-        />
-      )}
+        {showModal && (
+          <QuizSetup
+            quizOption={quizOption}
+            setQuizOption={setQuizOption}
+            setQuizStatus={setQuizStatus}
+            toggleShowModal={toggleShowModal}
+          />
+        )}
 
-      <img className='blob-bottom' src='/2.svg' alt='Bottom Blob' />
-    </main>
+        <img className='blob-bottom' src='/2.svg' alt='Bottom Blob' />
+      </main>
+
+      <footer>
+        <p>Created with 💜 by <a href="https://github.com/amy0h" target="_blank">Amy</a></p>
+      </footer>
+    </div>
   );
 }
 
